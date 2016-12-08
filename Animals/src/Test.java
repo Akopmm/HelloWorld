037530e58f9375014d185d55e93fb1a92144194a
Akopmm <Akop.babadzhanyan@gmail.com>
2016-12-08 17:38:51 +0300

    abstract(no_warnings)

--- Animals/src/Test.java
+++ Animals/src/Test.java
@@ -1,9 +1,7 @@
 
 public class Test {
 public static void main(String[] args){
-	Animal A=new Animal();
 	Wolf B=new Wolf();
-	System.out.println(A.getName());
 	System.out.println(B.getName());
 }
 }
