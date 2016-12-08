5551d228a8a4d8cec80d8462afd21d7cd0345d0d
Akopmm <Akop.babadzhanyan@gmail.com>
2016-12-08 16:40:32 +0300

    first

---
+++ Animals/src/Animal.java
@@ -0,0 +1,17 @@
+
+public class Animal {
+private int size=0;
+private String name="Animal";
+public String getName(){
+	return name;
+}
+public void setName(String n){
+	name=n;
+}
+public int getSize(){
+	return size;
+}
+public void setSize(int s){
+	size=s;
+}
+}
---
+++ Animals/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+public static void main(){
+	Animal A=new Animal();
+	Wolf B=new Wolf();
+	A.getName();
+	B.getName();
+	System.out.println(A.getName());
+	System.out.println(B.getName());
+}
+}
