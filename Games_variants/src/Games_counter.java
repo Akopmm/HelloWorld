e09f048fc0c8ee2e070dfbe3decff7b8fb5af10f
Akopmm <Akop.babadzhanyan@gmail.com>
2016-12-08 12:02:06 +0300

    3

---
+++ Games_variants/src/Games_counter.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Games_counter {
+public static void main(String[] args){
+	String s="";
+	s=s+StringUtils.repeat("1", 2);
+	System.out.println(s);
+}
+}
--- HelloWorld1/src/HelloWorld1.java
+++
@@ -1,8 +0,0 @@
-
-public class HelloWorld1 {
-public static void main(String[] args){
-	System.out.println("Hello, World!");
-	System.out.println("Hi!");
-	System.out.println("Hello!");
-}
-}
