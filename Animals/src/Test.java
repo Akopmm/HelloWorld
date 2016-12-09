f04a016bfb96eb5c08c1e9ac288cc1a666f3c937
Akopmm <Akop.babadzhanyan@gmail.com>
2016-12-09 10:31:12 +0300

    20161209 10:31

--- Animals/src/Test.java
+++ Animals/src/Test.java
@@ -1,7 +1,17 @@
-
+import java.util.*;
 public class Test {
 public static void main(String[] args){
 	Wolf B=new Wolf();
 	System.out.println(B.getName());
+	ArrayList<Object> AL = new ArrayList<Object>();
+	AL.add(B);
+	AL.add(B);
+	System.out.println(AL.size());
+	ArrayList<String> Str = new ArrayList<String>();
+	Str.add("1");
+	Str.add("second");
+	for (String s: Str){
+		System.out.print(s+" ");
+	}
 }
 }
